b0aab03dbb497c80395b9ba1678657705dc099af
Brian Torres-Gil <brian@ixi.us>
2022-08-09 14:13:14 -0700

    setup.sh - make more generic for other OS's

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
-#!/bin/sh
+#!/bin/zsh
 
 ## Create symlinks
 
-cd ~/.dotfiles
+cd "$HOME/.dotfiles" || exit
 # Make all the directories
-find . -type d | cut -c3- | xargs -I {} mkdir -p ~/{}
+find . -type d | cut -c3- | xargs -I {} mkdir -p "$HOME/{}"
 # Symlink all the files (except .git and setup.sh)
-find . -type f | cut -c3- | grep -vE '^(\.git|setup\.sh)' | xargs -I {} ln -s ~/.dotfiles/{} ~/{}
+find . -type f | cut -c3- | grep -vE '^(\.git|setup\.sh)' | xargs -I {} ln -s "$HOME/.dotfiles/{}" "$HOME/{}"
 
 ## Setup iterm2 preferences
 
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2_profile"
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+if [ "$(uname)" = "Darwin" ]; then
+  # Specify the preferences directory
+  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.dotfiles/iterm2_profile"
+  # Tell iTerm2 to use the custom preferences in the directory
+  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+fi
 
