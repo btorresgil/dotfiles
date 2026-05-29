f31e2d8d7f669d691699837b8b7679c54177a881
Brian Conklin <brian@dralth.com>
2026-05-29 09:20:16 -0700

    Install Lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,2 @@
-
--- Map ctrl-c to system clipboard
-vim.opt.clipboard = "unnamedplus"
-vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy selection to system clipboard" })
-
--- 
-vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy selection to system clipboard" })
-vim.keymap.set("n", "<leader>y", '"+y', { desc = "Copy motion to system clipboard" })
-vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Copy line to system clipboard" })
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
